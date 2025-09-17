637174432970d9c2c1ba0f10e927404e4bdd785d
colten995 <Cgoetz9919@conestogac.on.ca>
2025-09-17 00:06:01 -0400

    added a css file with styles for the container div and the boxes. container uses flexbox to create a 16x16 grid. Added code to generate the div boxes in the grid. Added an event listener to change the color of the boxes when the user hovers over them

--- etchasketch.js
+++ etchasketch.js
@@ -1,1 +1,32 @@
-alert("Hello world");
+const numOfRows = 16;
+const numOfColumns = 16;
+
+const containerDiv = document.getElementById("container");
+
+let numOfBoxes = 0;
+
+let grid = [,];
+
+for(let i = 0; i < numOfColumns; i++)
+{
+    for(let j = 0; j < numOfRows; j++)
+    {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        console.log(box);
+        box.addEventListener("mouseover", function(e){
+            e.target.style.background = "blue";
+        })
+        grid[i,j] = box;
+        containerDiv.appendChild(box);
+        numOfBoxes++;
+    }
+}
+
+
+// grid.forEach((box, row) => {
+//     box = document.createElement("div");
+// })
+
+console.log(grid);
+console.log(numOfBoxes)
